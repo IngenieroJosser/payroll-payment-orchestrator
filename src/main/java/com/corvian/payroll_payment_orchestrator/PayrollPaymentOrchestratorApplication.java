249cccde49a188f44747427e6d6cdb11cb77152a
threadstudio <cordobarivasjosser@gmail.com>
2026-05-18 01:23:19 -0500

    chore: configurando el proyecto

---
+++ src/main/java/com/corvian/payroll_payment_orchestrator/PayrollPaymentOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.corvian.payroll_payment_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollPaymentOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollPaymentOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corvian/payroll_payment_orchestrator/PayrollPaymentOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corvian.payroll_payment_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollPaymentOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
